11897734e7ab56f0505fb8c9d5aae8c04c35bade
NateeSaetae <168469845+NateeSaetae@users.noreply.github.com>
2025-04-21 02:05:36 +0700

    perceptron_model

--- web-ai-demo/src/AI_Models/trainPerceptron.js
+++ web-ai-demo/src/AI_Models/trainPerceptron.js
@@ -1,11 +1,15 @@
-// src/utils/perceptron.js
 export function trainPerceptron(dataset, learningRate = 0.1, epochs = 10) {
   let weights = [0, 0];
   let bias = 0;
 
   const activation = (z) => (z >= 0 ? 1 : 0);
 
+  const weightsPerEpoch = []; // เก็บค่าถ่วงน้ำหนักแต่ละรอบ
+  const errorsPerEpoch = [];  // เก็บค่า error รวมแต่ละ epoch
+
   for (let epoch = 0; epoch < epochs; epoch++) {
+    let totalError = 0;
+
     for (let { x1, x2, y } of dataset) {
       const z = weights[0] * x1 + weights[1] * x2 + bias;
       const pred = activation(z);
@@ -14,8 +18,20 @@ export function trainPerceptron(dataset, learningRate = 0.1, epochs = 10) {
       weights[0] += learningRate * error * x1;
       weights[1] += learningRate * error * x2;
       bias += learningRate * error;
+
+      totalError += Math.abs(error);
     }
+
+    // clone ค่า weights เก็บไว้ใน array
+    weightsPerEpoch.push([...weights]);
+    errorsPerEpoch.push(totalError);
   }
 
-  return { weights, bias, predict: (x1, x2) => activation(weights[0]*x1 + weights[1]*x2 + bias) };
+  return {
+    weights,
+    bias,
+    predict: (x1, x2) => activation(weights[0] * x1 + weights[1] * x2 + bias),
+    weightsPerEpoch,
+    errorsPerEpoch
+  };
 }
--- web-ai-demo/src/App.js
+++ web-ai-demo/src/App.js
@@ -1,8 +1,9 @@
-// src/App.js หรือหน้าอื่นๆ
-import React, { useEffect } from "react";
+import React, { useEffect , useState } from "react";
 import { trainPerceptron } from "./AI_Models/trainPerceptron";
+import ResultChart from "./Chart/ResultChart"
 
 function App() {
+  const [errorPerEpoch, setErrorPerEpoch] = useState([]);
   useEffect(() => {
     const data = [
       { x1: 0, x2: 0, y: 0 },
@@ -15,12 +16,21 @@ function App() {
     console.log("Weights:", model.weights);
     console.log("Bias:", model.bias);
     console.log("Predict (1, 1):", model.predict(1, 1));
+    const errors = model.errorsPerEpoch.map((item) => (item)); //[3, 2.5, 1.8, 1.2, 0.7, 0.2, 0.1]
+    setErrorPerEpoch(errors);
   }, []);
+
+   /*useEffect(() => {
+    // จำลอง error ในแต่ละ epoch จาก Perceptron
+    const errors = model.map((item) => (item.weights)); //[3, 2.5, 1.8, 1.2, 0.7, 0.2, 0.1]
+    setErrorPerEpoch(errors);
+  }, []);*/
 
   return (
     <div className="App">
       <h1>🧠 Perceptron AI Demo</h1>
       <p>เปิด Console เพื่อดูผลลัพธ์</p>
+      <ResultChart dataPoints={errorPerEpoch} />
     </div>
   );
 }
